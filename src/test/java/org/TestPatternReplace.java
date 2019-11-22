bd3c4835407a3a06dfe4cbd96298ffd578937076
Hong Her <hongher@Hong-Hers-MacBook-Pro.local>
2019-11-22 17:40:46 -0500

    Finish regex method.

--- src/test/java/org/TestPatternReplace.java
+++ src/test/java/org/TestPatternReplace.java
@@ -1,7 +1,15 @@
 package org;
 
+import io.cucumber.datatable.dependency.com.fasterxml.jackson.core.type.TypeReference;
+import io.cucumber.datatable.dependency.com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
 
+import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -11,20 +19,51 @@ public class TestPatternReplace
     @Test
     public void test() {
         String json = "{\"tenant\":\"Default IT Tenant blUs\",\"creator\":\"Default IT Creator YRR2\",\"workspaces\":[\"Default IT Workspaces 1\",\"Default IT Workspaces 2\"], \"anotherone\":[\"Default IT anotherone\"]}";
+        System.out.println( getKVPFromString( json ));
+    }
+
+    public ArrayList<String> getKVPFromString(String json) {
         Pattern jsonPattern = Pattern.compile("\"([\\w]*)\":\"([\\w\\s])*\"");
         Pattern jsonPatternList = Pattern.compile( "\"([\\w]*)\":\\[(\"([\\w\\s]*)\"(,)?)+]");
         Matcher matcher = jsonPattern.matcher( json );
         Matcher matcher1 = jsonPatternList.matcher( json );
-        System.out.println( "Start:" );
+        ArrayList<String> returnList = new ArrayList<String>(  );
 
-        for (int i = 1; i <= matcher.groupCount(); i++)
+        for (int i = 0; i < matcher.groupCount(); i++)
         {
-            matcher.find( i );
-            System.out.println( matcher.group() );
+            matcher.find(  );
+            returnList.add( matcher.group() );
+        }
+        for (int i = 0; i < matcher.groupCount(); i++)
+        {
+            matcher1.find( );
+            returnList.add( matcher1.group() );
         }
 
-        System.out.println("Start: 1");
-            matcher1.find();
-            System.out.println( matcher1.groupCount());
+        return returnList;
+    }
+
+    @Test
+    public void testingJsonMapper()
+    {
+        try
+        {
+            ObjectMapper mapper = new ObjectMapper();
+            String jsonString = "{\"tenant\":\"Default IT Tenant blUs\",\"creator\":\"Default IT Creator YRR2\"}";
+
+            String anotherone = "\"workspaces\":[\"Default IT Workspaces 1\",\"Default IT Workspaces 2\"], \"anotherone\":[\"Default IT anotherone\"]";
+
+            Map<String, Object> jsonMap  = mapper.readValue( anotherone,
+                    new TypeReference<Map<String, Array>>()
+                    {
+                    } );
+
+            System.out.println( jsonMap );
+        }
+        catch (
+                IOException ie )
+        {
+            ie.printStackTrace();
+        }
     }
 }
