331226fe52244ba783fe4b42a145bca7638b5ceb
Ubuntu 18.04 LTS <hongiaher@gmail.com>
2021-03-08 13:43:50 -0500

    Adding samsung gen.

--- src/test/java/org/robot/Mouse.java
+++ src/test/java/org/robot/Mouse.java
@@ -1,2 +1,25 @@
-package org.robot;public class Mouse {
+package org.robot;
+
+import com.google.common.util.concurrent.Uninterruptibles;
+import org.junit.After;
+import org.junit.Test;
+
+import java.awt.*;
+import java.awt.event.InputEvent;
+import java.util.concurrent.TimeUnit;
+
+public class Mouse {
+
+    @Test
+    public void moveMouse() throws AWTException {
+        Robot robot = new Robot();
+        robot.mouseMove(3300,400);
+        robot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+        robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+        Uninterruptibles.sleepUninterruptibly(3, TimeUnit.SECONDS);
+        robot.mouseMove(3300,250);
+
+
+    }
+
 }
