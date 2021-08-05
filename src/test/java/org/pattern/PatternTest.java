1fd440df2055b4cf58c324984c9205660c487794
Hong Her <hong.her@softvision.com>
2021-08-05 11:04:07 -0400

    Updaing 080521

--- src/test/java/org/pattern/PatternTest.java
+++ src/test/java/org/pattern/PatternTest.java
@@ -1,9 +1,13 @@
 package org.pattern;
 
+import org.apache.commons.lang3.StringUtils;
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class PatternTest {
 
@@ -13,5 +17,17 @@ public class PatternTest {
         Pattern p = Pattern.compile("^([\\d]{4})$");
         Matcher m = p.matcher(testString);
         System.out.println(m.matches());
+    }
+
+    @Test
+    public void test1() {
+        String testString= "%[afl;kje , afjeafje, gggjeig,eakefejf]";
+        Pattern p = Pattern.compile("%\\[(.)*]");
+        Matcher m = p.matcher(testString);
+        System.out.println(m.matches());
+        String newString = testString.replace("%[", "");
+        String[] stringArray = StringUtils.split(newString.replace("]", ""), ",");
+        List<String> stringList = Arrays.stream(stringArray).map(String::trim).collect(Collectors.toList());
+        System.out.println(stringList);
     }
 }
